c82082f32c52a3188ead114a2b02a99af021df66
almudenadomenech <violeta_33@hotmail.es>
2024-05-24 14:29:09 +0200

    componente e interfacevestidos

---
+++ Frontend/src/app/interfaces/vestido.model.ts
@@ -0,0 +1,11 @@
+export interface Vestido {
+
+    id: number;
+    model: string;
+    coleccion: string;
+    silueta: string;
+    detalle: string;
+    price: number;
+    photoUrl: string;
+
+}
--- Frontend/src/app/vestidos-list/vestidos-list.component.ts
+++ Frontend/src/app/vestidos-list/vestidos-list.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Vestido } from '../interfaces/vestido.model';
 
 @Component({
   selector: 'app-vestidos-list',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, RouterLink],
   templateUrl: './vestidos-list.component.html',
   styleUrl: './vestidos-list.component.css'
 })
-export class VestidosListComponent {
+export class VestidosListComponent implements OnInit {
+
+  vestidos: Vestido [] = [];
+
+  constructor(private httpClient: HttpClient){}
+
+  ngOnInit(): void {
+   this.httpClient.get<Vestido[]>('http://localhost:3000/vestidos')
+   .subscribe(vestidos => this.vestidos = vestidos);
+  }
 
 }
