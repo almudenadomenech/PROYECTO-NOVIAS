76000a8cf5761d30a04858372579248684de8ae2
almudenadomenech <violeta_33@hotmail.es>
2024-05-24 14:26:45 +0200

    modificacion letra

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { FooterComponent } from "./footer/footer.component";
+
+
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavbarComponent, FooterComponent, RouterLink]
 })
 export class AppComponent {
   title = 'Frontend';
