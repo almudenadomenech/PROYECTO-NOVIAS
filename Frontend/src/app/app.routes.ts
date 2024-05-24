1e1c9d4f2f1b8c1df378d1284d3f946caae0f32c
almudenadomenech <violeta_33@hotmail.es>
2024-05-24 14:28:12 +0200

    subida de fotos y rutas

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { VestidosDetailComponent } from './vestidos-detail/vestidos-detail.component';
+import { VestidosListComponent } from './vestidos-list/vestidos-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'vestidos',
+        component: VestidosListComponent
+    },
+    {
+        path: 'vestidos/:id/detail',
+        component: VestidosDetailComponent
+    },
+    
+];
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { join } from 'path';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const config = new DocumentBuilder().setTitle("novias").build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
+
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
   await app.listen(3000);
 }
-bootstrap();
+bootstrap() ;
