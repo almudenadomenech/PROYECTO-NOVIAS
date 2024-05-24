16722669289b8928fad886b07b9e43da36730210
almudenadomenech <violeta_33@hotmail.es>
2024-05-24 14:27:26 +0200

    modificado para backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { VestidosController } from './vestidos/vestidos.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Vestidos } from './vestidos/vestidos.model';
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost', 
+      port: 3306,
+      username: 'root',
+      password: 'admin',
+      database: 'novias',
+      entities: [Vestidos],
+      synchronize: true, 
+      logging: true
+    }),
+    TypeOrmModule.forFeature([Vestidos])
+  ],
+  controllers: [AppController, VestidosController],
   providers: [AppService],
 })
 export class AppModule {}
