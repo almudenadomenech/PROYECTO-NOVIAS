5aa85ce7b19ad9e4c4ba82cf22ab6cc92ff6a845
almudenadomenech <violeta_33@hotmail.es>
2024-06-04 14:06:57 +0200

    rutas añadidas

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,10 +4,23 @@ import { VestidosDetailComponent } from './vestidos-detail/vestidos-detail.compo
 import { VestidosListComponent } from './vestidos-list/vestidos-list.component';
 import { VestidosFormComponent } from './vestidos-form/vestidos-form.component';
 import { BookingFormComponent } from './booking-form/booking-form.component';
+import { BookingListComponent } from './booking-list/booking-list.component';
+import { BookingDetailComponent } from './booking-detail/booking-detail.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { AccountFormComponent } from './account-form/account-form.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
+
     {
         path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'home',
         component: HomeComponent
     },
     {
@@ -23,8 +36,48 @@ export const routes: Routes = [
         component: VestidosFormComponent
     },
     {
+        path: 'vestidos/:id/update',
+        component: VestidosFormComponent
+    },
+    {
         path:'booking',
+        component: BookingListComponent
+    },
+    {
+        path: 'booking/:id/detail',
+        component: BookingDetailComponent
+    },
+    {
+        path: 'booking/:id/form',
         component: BookingFormComponent
+    },
+    {
+        path: 'user',
+        component: UserListComponent
+    },
+    {
+        path: 'user/:id/detail',
+        component: UserDetailComponent
+    },
+    {
+        path: 'user/profile',
+        component: UserProfileComponent
+    },
+    {
+        path: 'user/:id/profile',
+        component: UserProfileComponent
+    },
+    {
+        path: 'account',
+        component: AccountFormComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
     }
     
 ];
