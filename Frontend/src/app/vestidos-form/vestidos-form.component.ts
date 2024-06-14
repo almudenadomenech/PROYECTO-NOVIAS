2fde90837375e1316cdc9cca4380f67f2d8dee5b
almudenadomenech <violeta_33@hotmail.es>
2024-06-14 12:06:55 +0200

    modificaciones en los componentes de vestidos

--- Frontend/src/app/vestidos-form/vestidos-form.component.ts
+++ Frontend/src/app/vestidos-form/vestidos-form.component.ts
@@ -1,9 +1,10 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+/*  import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
 import { Vestido } from '../interfaces/vestido.model';
+import { Category } from '../interfaces/category.model';
 
 @Component({
   selector: 'app-vestidos-form',
@@ -14,21 +15,26 @@ import { Vestido } from '../interfaces/vestido.model';
 })
 export class VestidosFormComponent implements OnInit {
 
+  category: Category | undefined;
+  vestido: Vestido | undefined;
+  categories: Category[] = [];
+
   vestidosForm = new FormGroup({
 
     id: new FormControl(),
-   
+    model: new FormControl(''),
     coleccion: new FormControl(''),
     price: new FormControl(0, [Validators.min(500), Validators.max(10000)]),
     photoUrl: new FormControl(''),
-    descripcion: new FormControl(''),
+    description: new FormControl(''),
     corte: new FormControl(''),
     escote: new FormControl(''),
     tipoCola: new FormControl(''),
     tejidos: new FormControl(''),
     espalda: new FormControl(''),
     talle: new FormControl(''),
-    tallas: new FormControl('')
+    tallas: new FormControl(''),
+   
     
   });
 
@@ -38,6 +44,7 @@ export class VestidosFormComponent implements OnInit {
   photoFile: File | undefined;
   photoPreview: string | undefined;
   showConfirmMessage = false;
+ 
 
   constructor(
     private httpClient : HttpClient,
@@ -45,6 +52,9 @@ export class VestidosFormComponent implements OnInit {
   ){}
   
   ngOnInit(): void {
+
+    this.loadCategories();
+
    this.activatedRoute.params.subscribe(params => {
     let id = params['id'];
     if (!id)
@@ -56,18 +66,19 @@ export class VestidosFormComponent implements OnInit {
 
       this.vestidosForm.reset({
         id: vestido.id,
-       
+        model: vestido.model,
         coleccion: vestido.coleccion,
         price: vestido.price,
         photoUrl: vestido.photoUrl,
-        descripcion: vestido.descripcion,
+        description: vestido.description,
         corte: vestido.corte,
         escote: vestido.escote,
         tipoCola: vestido.tipoCola,
         tejidos: vestido.tejidos,
         espalda: vestido.espalda,
         talle: vestido. talle,
-        tallas: vestido.tallas
+        tallas: vestido.tallas,
+        
 
       });
     });
@@ -83,6 +94,12 @@ export class VestidosFormComponent implements OnInit {
    });
   }
 
+  loadCategories(): void {
+    this.httpClient.get<Category[]>('http://localhost:3000/category')
+      .subscribe(categories => {
+        this.categories = categories;
+      });
+  }
   onFileChange(event: Event){
      let target = event.target as HTMLInputElement;
 
@@ -104,7 +121,7 @@ export class VestidosFormComponent implements OnInit {
     formData.append('coleccion', this.vestidosForm.get('coleccion')?.value ?? '');
     formData.append('price', this.vestidosForm.get('price')?.value + '');
     formData.append('photoUrl', this.vestidosForm.get('photoUrl')?.value ?? '');
-    formData.append('descripcion', this.vestidosForm.get('descripcion')?.value ?? '');
+    formData.append('description', this.vestidosForm.get('description')?.value ?? '');
     formData.append('corte', this.vestidosForm.get('corte')?.value ?? '');
     formData.append('escote', this.vestidosForm.get('escote')?.value ?? '');
     formData.append('tipoCola', this.vestidosForm.get('tipoCola')?.value ?? '');
@@ -112,7 +129,7 @@ export class VestidosFormComponent implements OnInit {
     formData.append('espalda', this.vestidosForm.get('espalda')?.value ?? '');
     formData.append('talle', this.vestidosForm.get('talle')?.value ?? '');
     formData.append('tallas', this.vestidosForm.get('tallas')?.value ?? '');
-
+    //formData.append('categoryId', this.vestidosForm.get('category')?.value ?? 0); // Añadir categoryId
     if (this.photoFile) formData.append('file', this.photoFile);
 
     if (this.isUpdate) {
@@ -122,6 +139,7 @@ export class VestidosFormComponent implements OnInit {
           this.photoFile = undefined;
           this.photoPreview = undefined;
           this.vestidos = vestido;
+          this.category = this.category;
           this.showConfirmMessage = true;
         },);
 
@@ -131,10 +149,176 @@ export class VestidosFormComponent implements OnInit {
           this.photoFile = undefined;
           this.photoPreview = undefined;
           this.vestidos = vestido;
+         
           this.showConfirmMessage = true;
         });
     }
   }
+  compareObjects(o1: any, o2: any): boolean {
+    if (o1 && o2) {
+      return o1.id === o2.id;
+    } else {
+      return o1 === o2;
+    }
+  }
+}
+ 
+
+
+ */
+
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
+import { Vestido } from '../interfaces/vestido.model';
+import { Category } from '../interfaces/category.model';
+import { VestidosService } from '../services/vestidos.service';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-vestidos-form',
+  standalone: true,
+  imports: [ReactiveFormsModule,RouterLink, HttpClientModule, NgbAlert],
+  templateUrl: './vestidos-form.component.html',
+  styleUrl: './vestidos-form.component.css'
+})
+export class VestidosFormComponent implements OnInit {
+
+  category: Category | undefined;
+  vestido: Vestido | undefined;
+  categories: Category[] = [];
+
+  vestidosForm = new FormGroup({
+    id: new FormControl(),
+    model: new FormControl(''),
+    coleccion: new FormControl(''),
+    price: new FormControl(0, [Validators.min(500), Validators.max(10000)]),
+    photoUrl: new FormControl(''),
+    description: new FormControl(''),
+    corte: new FormControl(''),
+    escote: new FormControl(''),
+    tipoCola: new FormControl(''),
+    tejidos: new FormControl(''),
+    espalda: new FormControl(''),
+    talle: new FormControl(''),
+    tallas: new FormControl(''),
+    categoryId: new FormControl(), 
+  });
+
+  vestidos: Vestido | undefined;
+  isUpdate: boolean = false;
+  isDelete: boolean = false;
+  photoFile: File | undefined;
+  photoPreview: string | undefined;
+  showConfirmMessage = false;
+
+  constructor(
+    private vestidosService: VestidosService,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.loadCategories();
+
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id'];
+      if (!id)
+        return;
+
+      this.vestidosService.getVestido(id)
+        .subscribe(vestido => {
+          this.isUpdate = true;
+
+          this.vestidosForm.reset({
+            id: vestido.id,
+            model: vestido.model,
+            coleccion: vestido.coleccion,
+            price: vestido.price,
+            photoUrl: vestido.photoUrl,
+            description: vestido.description,
+            corte: vestido.corte,
+            escote: vestido.escote,
+            tipoCola: vestido.tipoCola,
+            tejidos: vestido.tejidos,
+            espalda: vestido.espalda,
+            talle: vestido.talle,
+            tallas: vestido.tallas,
+            categoryId: vestido.categoryId // Asignar categoryId
+          });
+        });
+    });
+
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id'];
+      if (id)
+        this.vestidosService.getVestido(id)
+          .subscribe(vestido => {
+            this.isDelete = true;
+          });
+    });
+  }
+
+  loadCategories(): void {
+    this.vestidosService.getCategories()
+      .subscribe(categories => {
+        this.categories = categories;
+      });
+  }
+
+  onFileChange(event: Event) {
+    let target = event.target as HTMLInputElement;
+
+    if (target.files !== null && target.files.length > 0) {
+      this.photoFile = target.files[0];
+
+      let reader = new FileReader();
+      reader.onload = event => this.photoPreview = reader.result as string;
+      reader.readAsDataURL(this.photoFile);
+    }
+  }
+
+  save(): void {
+    let formData = new FormData();
+
+    formData.append('id', this.vestidosForm.get('id')?.value ?? 0);
+    formData.append('model', this.vestidosForm.get('model')?.value ?? '');
+    formData.append('coleccion', this.vestidosForm.get('coleccion')?.value ?? '');
+    formData.append('price', this.vestidosForm.get('price')?.value + '');
+    formData.append('photoUrl', this.vestidosForm.get('photoUrl')?.value ?? '');
+    formData.append('description', this.vestidosForm.get('description')?.value ?? '');
+    formData.append('corte', this.vestidosForm.get('corte')?.value ?? '');
+    formData.append('escote', this.vestidosForm.get('escote')?.value ?? '');
+    formData.append('tipoCola', this.vestidosForm.get('tipoCola')?.value ?? '');
+    formData.append('tejidos', this.vestidosForm.get('tejidos')?.value ?? '');
+    formData.append('espalda', this.vestidosForm.get('espalda')?.value ?? '');
+    formData.append('talle', this.vestidosForm.get('talle')?.value ?? '');
+    formData.append('tallas', this.vestidosForm.get('tallas')?.value ?? '');
+    formData.append('categoryId', this.vestidosForm.get('categoryId')?.value ?? ''); // Añadir categoryId
+    if (this.photoFile) formData.append('file', this.photoFile);
+
+    if (this.isUpdate) {
+      const id = this.vestidosForm.get('id')?.value;
+      this.vestidosService.updateVestido(id, formData)
+        .subscribe(vestido => {
+          this.photoFile = undefined;
+          this.photoPreview = undefined;
+          this.vestidos = vestido;
+          this.category = this.category;
+          this.showConfirmMessage = true;
+        });
+
+    } else {
+      this.vestidosService.createVestido(formData)
+        .subscribe(vestido => {
+          this.photoFile = undefined;
+          this.photoPreview = undefined;
+          this.vestidos = vestido;
+          this.showConfirmMessage = true;
+        });
+    }
+  }
+
   compareObjects(o1: any, o2: any): boolean {
     if (o1 && o2) {
       return o1.id === o2.id;
--- Frontend/src/app/vestidos-list/vestidos-list.component.ts
+++ Frontend/src/app/vestidos-list/vestidos-list.component.ts
@@ -1,34 +1,12 @@
-/* import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { Vestido } from '../interfaces/vestido.model';
-
-@Component({
-  selector: 'app-vestidos-list',
-  standalone: true,
-  imports: [HttpClientModule, RouterLink],
-  templateUrl: './vestidos-list.component.html',
-  styleUrl: './vestidos-list.component.css'
-})
-export class VestidosListComponent implements OnInit {
-
-  vestidos: Vestido [] = [];
-
-  constructor(private httpClient: HttpClient){}
-
-  ngOnInit(): void {
-   this.httpClient.get<Vestido[]>('http://localhost:3000/vestidos')
-   .subscribe(vestidos => this.vestidos = vestidos);
-  }
-
-} */
+ 
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit, TemplateRef, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { Vestido } from '../interfaces/vestido.model';
 import { ImageService } from '../shared/image.service';
 import { NgbAlert, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AuthenticationService } from '../authentication/authentication.service';
+import { Category } from '../interfaces/category.model';
 
 @Component({
   selector: 'app-vestidos-list',
@@ -40,13 +18,16 @@ import { AuthenticationService } from '../authentication/authentication.service'
 export class VestidosListComponent implements OnInit {
 
   vestidos: Vestido [] = [];
+  categories: Category[] = [];
   baseUrl: string;
   isAdmin = false;
   showConfirmMessage = false;
   private modalService = inject(NgbModal);
 
   constructor(private httpClient: HttpClient, private imageService: ImageService,
-    private authService: AuthenticationService) {
+    private authService: AuthenticationService,
+    private activatedRoute: ActivatedRoute,
+  private router: Router) {
     this.authService.isAdmin.subscribe(isAdmin => this.isAdmin = isAdmin)
     this.baseUrl = this.imageService.getBaseUrl();
   }
@@ -62,14 +43,27 @@ export class VestidosListComponent implements OnInit {
       }
     });
   }
-  ngOnInit(): void {
+   ngOnInit(): void {
+
+    this.httpClient.get<Category[]>('http://localhost:3000/category')
+    .subscribe(categories => this.categories = categories);
     this.loadVestidos();
-  }
-  
+    //this.categoryId();
+  } 
+
+ 
+
   loadVestidos(): void{
     this.httpClient.get<Vestido[]>('http://localhost:3000/vestidos')
-      .subscribe(vestidos => this.vestidos = vestidos);
+      .subscribe( vestidosFromBackend=> this.vestidos = vestidosFromBackend);
+
+   
   }
+  
+  /* categoryId(): void{
+    this.httpClient.get<Category[]>('http://localhost:3000/category/:id'  )
+    .subscribe(categories => this.categories = categories);
+  } */
 
   deleteById(vestido: Vestido){
    
@@ -80,3 +74,76 @@ export class VestidosListComponent implements OnInit {
       });
 }
 }
+ 
+
+/* import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Vestido } from '../interfaces/vestido.model';
+import { ImageService } from '../shared/image.service';
+import { NgbAlert, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AuthenticationService } from '../authentication/authentication.service';
+import { Category } from '../interfaces/category.model';
+
+
+@Component({
+  selector: 'app-vestidos-list',
+  templateUrl: './vestidos-list.component.html',
+  styleUrls: ['./vestidos-list.component.css']
+})
+export class VestidosListComponent implements OnInit {
+
+  vestidos: Vestido[] = [];
+  categories: Category[] = [];
+  baseUrl: string;
+  isAdmin = false;
+  showConfirmMessage = false;
+
+  constructor(
+    private httpClient: HttpClient,
+    private imageService: ImageService,
+    private authService: AuthenticationService,
+    private modalService: NgbModal,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
+    this.authService.isAdmin.subscribe(isAdmin => this.isAdmin = isAdmin);
+    this.baseUrl = this.imageService.getBaseUrl();
+  }
+
+  ngOnInit(): void {
+    this.loadVestidos();
+    this.loadCategories();
+  }
+
+  openModal(content: TemplateRef<any>, vestido: Vestido): void {
+    const modalRef = this.modalService.open(content, {
+      centered: true
+    });
+    modalRef.result.then(result => {
+      if (result === 'Aceptar') {
+        console.log('Ha pulsado borrar vestido');
+        this.deleteById(vestido);
+      }
+    });
+  }
+
+  loadVestidos(): void {
+    this.httpClient.get<Vestido[]>('http://localhost:3000/vestidos/')
+      .subscribe(vestidos => this.vestidos = vestidos);
+  }
+
+  loadCategories(): void {
+    this.httpClient.get<Category[]>('http://localhost:3000/categories')
+      .subscribe(categories => this.categories = categories);
+  }
+
+  deleteById(vestido: Vestido): void {
+    this.httpClient.delete<Vestido>('http://localhost:3000/vestidos/' + vestido.id)
+      .subscribe(() => {
+        this.showConfirmMessage = true;
+        this.loadVestidos();
+      });
+  }
+}
+ */
